3f224530f5290c50cc9341d156dbf10b1b1a2608
Shashwat Sharma <30342542+imsashwat@users.noreply.github.com>
2023-02-05 00:11:26 +0530

    update delete and getbyId via LIst not DB

--- src/main/java/com/lcwd/todo/Services/TodoService.java
+++ src/main/java/com/lcwd/todo/Services/TodoService.java
@@ -3,6 +3,9 @@ package com.lcwd.todo.Services;
 import com.lcwd.todo.models.Todo;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +22,43 @@ public class TodoService {
         todos.add(todo);
         // Logger.info("Todos {} ", this.todos);
         return todo;
+
+    }
+
+    public List<Todo> getAllTodo() {
+        return todos;
+    }
+
+    public Todo getTodo(int todoId) {
+        Todo todo = todos.stream().filter(t -> todoId == t.getId()).findAny().get();
+        return todo;
+    }
+
+    public Todo updateTodo(int todoId, Todo todo) {
+        List<Todo> newUpdateList = todos.stream().map(t -> {
+
+            if (t.getId() == todoId) {
+                // perform update
+                t.setTitle(todo.getTitle());
+                t.setContent(todo.getContent());
+                t.setStatus(todo.getStatus());
+                return t;
+
+            } else {
+                return t;
+            }
+
+        }).collect(Collectors.toList());
+
+        todos = newUpdateList;
+        todo.setId(todoId);
+        return todo;
+    }
+
+    // method for delete
+    public void deleteTodo(int todoId) {
+        List<Todo> newList = todos.stream().filter(t -> t.getId() != todoId).collect(Collectors.toList());
+        todos = newList;
 
     }
 
--- src/main/java/com/lcwd/todo/controllers/TodoController.java
+++ src/main/java/com/lcwd/todo/controllers/TodoController.java
@@ -1,14 +1,22 @@
 package com.lcwd.todo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lcwd.todo.Services.TodoService;
 import com.lcwd.todo.models.Todo;
+
+import java.util.List;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,14 +27,46 @@ public class TodoController {
 
     Logger logger = LoggerFactory.getLogger(TodoController.class);
 
+    @Autowired
+    private TodoService todoService;
+
+    Random random = new Random();
+
     @PostMapping
-
-    // responseENtitty se status vgerh
     public ResponseEntity<Todo> createTodoHandler(@RequestBody Todo todo) {
+        int id = random.nextInt(99999);
+        todo.setId(id);
 
-        logger.info("Create Todo");
         Todo todo1 = TodoService.createTodo(todo);
         return new ResponseEntity<>(todo1, HttpStatus.CREATED);
+    }
+
+    // get all todo
+    @GetMapping
+    public ResponseEntity<List<Todo>> getAllTodoHandler() {
+        List<Todo> allTodos = todoService.getAllTodo();
+        return new ResponseEntity<>(allTodos, HttpStatus.OK);
+    }
+
+    // getisngle todo
+    @GetMapping("/{todoId}")
+    public ResponseEntity<Todo> getSingleTodoHandler(@PathVariable int todoId) {
+        Todo todo = todoService.getTodo(todoId);
+        return ResponseEntity.ok(todo);
+    }
+
+    // update todo
+    @PutMapping("/{todoId}")
+    public ResponseEntity<Todo> updateTodoHandler(@RequestBody Todo todoWithNewDetails, @PathVariable int todoId) {
+        Todo todo = todoService.updateTodo(todoId, todoWithNewDetails);
+        return ResponseEntity.ok(todo);
+    }
+
+    // delete
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<String> deleteTodo(@PathVariable int todoId) {
+        todoService.deleteTodo(todoId);
+        return ResponseEntity.ok("successfully delete");
 
     }
 
